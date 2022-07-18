f2d62f0dff81f0bf9d26b3bdd903500f435895f6
jaymin-developer <kjmkjm822@naver.com>
2022-07-18 17:11:05 +0900

    fix/파일 경로 수정

--- components/signup/SignUp.compoent.tsx
+++ components/signup/SignUp.compoent.tsx
@@ -3,7 +3,7 @@ import { Modal } from "antd";
 import { useRouter } from "next/router";
 import { ChangeEvent, useState } from "react";
 import * as SignUp from "../../src/styles/signup/SignUp.styles";
-import { CREATE_USER } from "../../types/signup/SignUp.types";
+import { CREATE_USER } from "../../src/types/signup/SignUp.types";
 import { FuncButtonMain } from "../common/Button.component";
 
 const SignUpComponent = () => {
